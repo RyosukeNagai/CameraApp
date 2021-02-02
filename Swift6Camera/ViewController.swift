7f81a804badfa9681d22e49a4122ff7dd9d93900
vagrant <vagrant@localhost.localdomain>
2021-02-02 11:31:31 +0000

    first commit

---
+++ Swift6Camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6Camera
+//
+//  Created by 長井崚介 on 2021/02/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
